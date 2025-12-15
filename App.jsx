3175d407040b46e29972a1ea913754da0f0daf18
shoriful12win <128087926+shoriful12win@users.noreply.github.com>
2025-12-15 11:56:10 +0600

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import HeroSection from './component/Hero';
+import Navigation from './component/Navigation';
+const App = () => {
+  return  (
+    <div>
+      <Navigation/>
+      <HeroSection/>
+    </div>
+  
+  );
+};
+export default  App;
