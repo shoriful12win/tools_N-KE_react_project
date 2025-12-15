9482cd8e425483f47d18b3b69b3c8e3c4c627c93
shoriful12win <128087926+shoriful12win@users.noreply.github.com>
2025-12-15 11:58:17 +0600

    Add files via upload

---
+++ Hero.jsx
@@ -0,0 +1,32 @@
+const HeroSection = () =>{
+
+return(
+    <main className="hero">
+<div className="hero-contant">
+<h1>YOUR FEET DESERVE THE BEST</h1>
+<p>Stepping up the game! 🔥 These look incredible.
+Never settle for less because YOUR FEET DESERVE THE BEST
+
+</p>
+<div className="hero-btn">
+    <button>SHOP Now</button>
+    <button className="category">Category</button>
+</div>
+<div className="shopping">
+
+<p>Also Avaliable On</p>
+</div>
+<div className="coto-image">
+    <img src="/images/shops.png" alt="icon" />
+</div>
+</div>
+<div className="hero-image">
+<img src="/images/shoe_image.png" alt="" />
+</div>
+
+    </main>
+
+);
+
+};
+export default HeroSection;
